67b6f1bfb8d373465dee0826a32d50fc8ca1ec54
Samreen Jahan <jahansamreen370@gmail.com>
2026-02-26 07:13:47 +0530

    new

--- ds.js
+++ ds.js
@@ -148,3 +148,228 @@ function lengthofLongestSubstring(s){
     return maxlen;
 }
 console.log(lengthofLongestSubstring("abcabcbb"));
+
+// Q4)Given an array of numbers, move all 0's to the end while keeping non-0 values
+// in the same relative order
+
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+// void moveZeros(vector<int>&nums){
+//     int j=0, n=nums.size();
+//     for(int i=0; i<n; i++){
+//         if(nums[i]!=0){
+//             swap(nums[i],nums[j]);
+//             j++;
+//         }
+//     }
+// }
+
+// int main() {
+//     vector<int>nums={0,1,0,3,12};
+    
+//     moveZeros(nums);
+//     for(auto it: nums){
+//         cout<<it<<" ";
+//     }
+//     return 0;
+// }
+
+function moveZeros(nums){
+    let j=0, n=nums.length;
+    for(let i=0; i<n; i++){
+        if(nums[i]!=0){
+            [nums[i],nums[j]]=[nums[j], nums[i]];
+            j++;
+        }
+    }
+    return nums;
+}
+
+
+console.log(moveZeros([0,1,0,3,12]));
+
+// Q5.Determine whether two strings are anagrams by companring character frequency
+
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+// bool isAnagram(string a, string b){
+//     if(a.length()!=b.length())return false;
+//     vector<int>freq(26,0);
+//     for(int i=0; i<a.length(); i++){
+//         freq[a[i]-'a']++;
+//         freq[b[i]-'a']--;
+//     }
+//     for(auto it: freq){
+//         if(it!=0)return false;
+//     }
+//     return true;
+// }
+// int main() {
+//     string a="car";
+//     string b="rat";
+//     cout<<isAnagram(a,b);
+//     return 0;
+// }
+
+function isAnagram(s,t){
+    if(s.length != t.length)return false;
+    const freq= new Array(26).fill(0);
+    for(let i=0; i<s.length; i++){
+        freq[s.charCodeAt(i)-97]++;
+        freq[t.charCodeAt(i)-97]--;
+    }
+    for(let count of freq){
+        if(count !== 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log("isAnagram: ",isAnagram("listen","silent"));
+
+// Q6) Given the array of integer return the index of the second highest number
+
+// int f(vector<int>&v){
+//     int n=v.size();
+//     int maxi=INT_MIN;
+//     int sec_maxi=INT_MIN;
+//     int ans=-1;
+//     for(int i=0; i<n; i++){
+//         if(v[i]>maxi){
+//             maxi=v[i];
+//         }
+//     }
+    
+//     for(int i=0; i<n; i++){
+//         if(v[i]!=maxi && v[i]>sec_maxi)ans=i;
+//     }
+//     return ans;
+// }
+
+function f(v){
+    let maxi=-Infinity;
+    let sec_max=-Infinity;
+
+    let index=-1;
+    let n=v.length;
+    for(let i=0; i<n; i++){
+        if(v[i]>maxi){
+            maxi=v[i];
+        }
+        else if(v[i] !== maxi && v[i]>sec_max){
+            index=i;
+        }
+    }
+    return index;
+}
+console.log("sec_max: ",f([1,2,3,4,5,23,10]));
+
+// Q7) Remove duplicate character from a string while preserving the order of first 
+// appearacne
+
+// string f(string s){
+//     unordered_set<char>st;
+//     string res="";
+//     for(int i=0; i<s.length(); i++){
+//         if(st.find(s[i]) == st.end()){
+//             st.insert(s[i]);
+//             res+=s[i];
+//         }
+//     }
+//     return res;
+// }
+
+function f(s){
+    let res="";
+    let st =new Set();
+    for(let a of s){
+        if(!st.has(a)){
+            st.add(a);
+            res+=a;
+        }
+    }
+    return res;
+}
+
+console.log("remove_duplicate: ", f("programming"));
+
+// Q8)Return the maximum sum of any contiguous subarray
+
+function f(a){
+    let maxi=-Infinity;
+    let currSum=0;
+    for(let i=0; i<a.length; i++){
+        currSum+=a[i];
+        maxi=Math.max(maxi,currSum);
+    }
+    return maxi;
+}
+console.log("max_sum: ", f([1,2,3,4,5]));
+
+// Q9) Reverse the order of words in a sentence without using built-in split()
+function r(s){
+    let n=s.length;
+   
+    let res="";
+    for(let i=n-1; i>=0; i--){
+         let word="";
+    while(i>=0 && s[i]===' ')i--;
+    while(i>=0 && s[i]!==' '){
+        word= s[i]+word;
+        i--;
+    }
+    
+
+   if(word.length>0){
+    if(res.length>0){
+        res+=" ";
+    }
+    res+=word;
+   }
+}
+   return res;
+}
+console.log("reverse string order: ", r("I love coding!"));
+
+// Q10) From an array of integers, return all unique pairs that sum to a target value.
+
+// vector<vector<int>> twoSumPairs(vector<int>& nums, int target) {
+//     unordered_set<int> seen;
+//     set<pair<int,int>> result; // ensures uniqueness
+
+//     for (int num : nums) {
+//         int complement = target - num;
+
+//         if (seen.count(complement)) {
+//             int a = min(num, complement);
+//             int b = max(num, complement);
+//             result.insert({a, b});
+//         }
+//         seen.insert(num);
+//     }
+
+//     vector<vector<int>> ans;
+//     for (auto &p : result) {
+//         ans.push_back({p.first, p.second});
+//     }
+
+//     return ans;
+// }
+
+function twoSum(nums,target){
+    let st = new Set();
+    let  res= new Set();
+
+    for(let num of nums){
+
+        if(st.has(num)){
+            
+        }
+        st.add(num);
+    }
+}
+
+console.log(twoSum([1, 2, 3, 4, 5, 6, 7], 8));
